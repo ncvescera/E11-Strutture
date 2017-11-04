8e1ed096a091c3373a2b08b050b1dfdefd4973c8
ncvescera <ncvescera@mbp-di-nicolo>
2017-11-04 16:33:12 +0100

    Completato esercizio

--- main.c
+++ main.c
@@ -1,18 +1,50 @@
-/* 
- * File:   main.c
- * Author: ncvescera
- *
- * Created on 4 novembre 2017, 11.34
- */
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include "bool.h"
 
-/*
- * 
- */
+struct s_punto {
+    double x;
+    double y;
+};
+typedef struct s_punto punto;
+
+struct s_cerchio {
+    punto centro;
+    double ragggio;
+};
+typedef struct s_cerchio cerchio;
+
+double pointDist(punto p1, punto p2);
+bool isInCerchio(cerchio c, punto p);
+
 int main(int argc, char** argv) {
-
+    cerchio c = {
+        {2,0},
+        3
+    };
+    punto p = {3,0};
+    
+    printf("%d\n",isInCerchio(c,p));
+    
     return (EXIT_SUCCESS);
 }
 
+double pointDist(punto p1, punto p2) {
+    double dist;
+
+    dist = sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
+
+    return dist;
+}
+
+bool isInCerchio(cerchio c, punto p) {
+    bool result;
+
+    if (pointDist(c.centro, p) > c.ragggio)
+        result = FALSE;
+    else
+        result = TRUE;
+
+    return result;
+}
--- nbproject/Makefile-Debug.mk
+++ nbproject/Makefile-Debug.mk
@@ -21,8 +21,8 @@ FC=gfortran
 AS=as
 
 # Macros
-CND_PLATFORM=Cygwin-Windows
-CND_DLIB_EXT=dll
+CND_PLATFORM=GNU-MacOSX
+CND_DLIB_EXT=dylib
 CND_CONF=Debug
 CND_DISTDIR=dist
 CND_BUILDDIR=build
@@ -56,13 +56,13 @@ LDLIBSOPTIONS=
 
 # Build Targets
 .build-conf: ${BUILD_SUBPROJECTS}
-	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/e11strutture.exe
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/e11-strutture
 
-${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/e11strutture.exe: ${OBJECTFILES}
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/e11-strutture: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
-	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/e11strutture ${OBJECTFILES} ${LDLIBSOPTIONS}
+	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/e11-strutture ${OBJECTFILES} ${LDLIBSOPTIONS}
 
-${OBJECTDIR}/main.o: main.c 
+${OBJECTDIR}/main.o: main.c
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} "$@.d"
 	$(COMPILE.c) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.c
@@ -73,7 +73,6 @@ ${OBJECTDIR}/main.o: main.c
 # Clean Targets
 .clean-conf: ${CLEAN_SUBPROJECTS}
 	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
-	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/e11strutture.exe
 
 # Subprojects
 .clean-subprojects:
